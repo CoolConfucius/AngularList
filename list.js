c7766bef11aabd8b18469fe957c262b4831fe3cf
Benjamin Huang <coolconfucius@yahoo.com>
2015-09-14 12:43:51 -0700

    two inputs. no duplicated items on list

--- list.js
+++ list.js
@@ -1,12 +1,14 @@
 'use strict'; 
 var app = angular.module('testapp', []);
 
-app.controller('testCtrl', ['$scope', function($scope, $window) {
+app.controller('testCtrl', ['$scope', '$window', function($scope, $window) {
 
   $scope.list = [];
 
-  $scope.clickButton = function() {    
-    $scope.list.push($scope.input);
+  $scope.clickButton = function() {
+    if ($scope.list.indexOf($scope.input) === -1) {
+      $scope.list.push($scope.input);  
+    };    
     $scope.input = '';
   };
 
